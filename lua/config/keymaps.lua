b5aabe11002e03025f98b81c04845f3cd93e828b
Kaelyn Ferris <kaelyn@li-f3687dcc-353e-11b2-a85c-b8b3ed4bb92d.ibm.com>
2023-11-27 12:03:08 -0500

    Updated Neorg workspaces for work

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,34 +11,33 @@ vim.g.maplocalleader = ","
 --
 --  Couple toggle concealer with the conceallevel
 vim.keymap.set("n", "<leader>nt", function()
-  if vim.opt.conceallevel:get() == 2 then
-    vim.opt.conceallevel = 0
-  elseif vim.opt.conceallevel:get() == 0 then
-    vim.opt.conceallevel = 2
-  end
-  vim.cmd.Neorg("toggle-concealer")
+	if vim.opt.conceallevel:get() == 2 then
+		vim.opt.conceallevel = 0
+	elseif vim.opt.conceallevel:get() == 0 then
+		vim.opt.conceallevel = 2
+	end
+	vim.cmd.Neorg("toggle-concealer")
 end)
 
 -- Register neorg motions to whichkey
 local wk = require("which-key")
 wk.register({
-  ["<leader>n"] = {
-    name = "+Neorg",
-    w = {
-      name = "+workspace",
-      h = { "<cmd>Neorg workspace homelab<cr>", "Open homelab workspace" },
-      e = { "<cmd>Neorg workspace gmeclipse<cr>", "Open Eclipse Phase workspace" },
-      g = { "<cmd>Neorg workspace oldGods<cr>", "Open Old Gods of Appalachia workspace" },
-    },
-    j = {
-      name = "+journal",
-      c = { "<cmd>Neorg journal custom<cr>", "Custom journal" },
-      j = { "<cmd>Neorg journal template<cr>", "Template journal" },
-      t = { "<cmd>Neorg journal today<cr>", "Today journal" },
-      m = { "<cmd>Neorg journal tomorrow<cr>", "Tomorrow journal" },
-      y = { "<cmd>Neorg journal yesterday<cr>", "Yesterday journal" },
-    },
-    t = "Toggle concealer",
-    r = { "<cmd>Neorg return<cr>", "Exit neorg" },
-  },
+	["<leader>n"] = {
+		name = "+Neorg",
+		w = {
+			name = "+workspace",
+			w = { "<cmd>Neorg workspace work<cr>", "Open 'work' workspace" },
+			q = { "<cmd>Neorg workspace quantum<cr>", "Open Qauntum Wiki workspace" },
+		},
+		j = {
+			name = "+journal",
+			c = { "<cmd>Neorg journal custom<cr>", "Custom journal" },
+			j = { "<cmd>Neorg journal template<cr>", "Template journal" },
+			t = { "<cmd>Neorg journal today<cr>", "Today journal" },
+			m = { "<cmd>Neorg journal tomorrow<cr>", "Tomorrow journal" },
+			y = { "<cmd>Neorg journal yesterday<cr>", "Yesterday journal" },
+		},
+		t = "Toggle concealer",
+		r = { "<cmd>Neorg return<cr>", "Exit neorg" },
+	},
 })
--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -6,35 +6,33 @@
 --      flexibility.  The furture of organizing your life in Neovim
 --
 return {
-  "nvim-neorg/neorg",
-  build = ":Neorg sync-parsers",
-  dependencies = { "nvim-lua/plenary.nvim", "nvim-neorg/neorg-telescope" },
-  config = function()
-    require("neorg").setup({
-      load = {
-        ["core.defaults"] = {}, -- Loads default behaviour
-        ["core.concealer"] = {}, -- Adds pretty icons to your documents
-        ["core.dirman"] = { -- Manages Neorg workspaces
-          config = {
-            workspaces = {
-              testing = "~/notes/neorg-testing/",
-              homelab = "~/notes/homelab",
-              gmeclipse = "~/notes/eclipse-phase/",
-              oldGods = "~/notes/old-gods-of-appalachia/",
-            },
-            index = "index.norg",
-            default_workspaces = "testing",
-          },
-        },
-        ["core.export"] = {},
-        ["core.summary"] = {},
-        ["core.integrations.telescope"] = {},
-        -- ["core.keybinds"] = {
-        --   config = {
-        --     neorg_leader = ",",
-        --   },
-        -- },
-      },
-    })
-  end,
+	"nvim-neorg/neorg",
+	build = ":Neorg sync-parsers",
+	dependencies = { "nvim-lua/plenary.nvim", "nvim-neorg/neorg-telescope" },
+	config = function()
+		require("neorg").setup({
+			load = {
+				["core.defaults"] = {}, -- Loads default behaviour
+				["core.concealer"] = {}, -- Adds pretty icons to your documents
+				["core.dirman"] = { -- Manages Neorg workspaces
+					config = {
+						workspaces = {
+							quantum = "~/projects/notes/quantum-wiki/",
+							work = "~/projects/notes/work/",
+						},
+						index = "index.norg",
+						default_workspaces = "work",
+					},
+				},
+				["core.export"] = {},
+				["core.summary"] = {},
+				["core.integrations.telescope"] = {},
+				-- ["core.keybinds"] = {
+				--   config = {
+				--     neorg_leader = ",",
+				--   },
+				-- },
+			},
+		})
+	end,
 }
