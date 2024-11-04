863ca6f30d522b047fb84282ecdb1ba3a7692c5a
kaelynj <43348706+kaelynj@users.noreply.github.com>
2024-11-03 22:04:20 -0500

    Remove neorg plugin config, clean up errors

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -25,10 +25,13 @@ return {
 		--		},
 		win = {
 			border = "none", -- none, single, double, shadow
-			position = "bottom", -- bottom, top
-			margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]. When between 0 and 1, will be treated as a percentage of the screen size.
+--			position = "bottom", -- bottom, top
+--			margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]. When between 0 and 1, will be treated as a percentage of the screen size.
 			padding = { 1, 2, 1, 2 }, -- extra window padding [top, right, bottom, left]
-			winblend = 15, -- value between 0-100 0 for fully opaque and 100 for fully transparent
+      wo = {
+        winblend = 1000,
+      },
+--			winblend = 15, -- value between 0-100 0 for fully opaque and 100 for fully transparent
 			zindex = 1000, -- positive value to position WhichKey above other floating windows.
 		},
 		layout = {
